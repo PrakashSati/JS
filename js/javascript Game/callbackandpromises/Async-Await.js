e13f84b9e75429df1b9436cfa24373613bd766a2
PrakashSati <satip4411@gmail.com>
2026-02-24 11:23:29 +0530

    file completed

---
+++ js/javascript Game/callbackandpromises/Async-Await.js
@@ -0,0 +1,46 @@
+// async function hello() {
+//   console.log("hello");
+// }
+
+// function api() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("weather data");
+//       resolve(200);
+//     }, 2000);
+//   });
+// }
+
+// async function getWeatherData() {
+//   await api();
+// }
+
+function getData(dataId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve("success");
+    }, 2000);
+  });
+}
+
+// async function getAllData() {
+//   console.log("Data1....");
+//   await getData(1);
+//   console.log("Data2....");
+//   await getData(2);
+//   console.log("Data3....");
+//   await getData(3);
+//   console.log("success");
+// }
+
+// IIFE == It is used for immediately run function without calling it. It is used only one time.
+(async function () {
+  console.log("Data1....");
+  await getData(1);
+  console.log("Data2....");
+  await getData(2);
+  console.log("Data3....");
+  await getData(3);
+  console.log("success");
+})();
--- js/javascript Game/callbackandpromises/promises.js
+++ js/javascript Game/callbackandpromises/promises.js
@@ -1,9 +1,19 @@
-const getPromise = () => {
-  new Promise((resolve, reject) => {
-    console.log("promise created");
-    resolve("success");
-  });
-};
+// const getPromise = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("I am a promise created");
+//     resolve("==> success");
+//     reject("==> failed");
+//   });
+// };
+
+// let promise = getPromise();
+// promise.then((res) => {
+//   console.log("promise fulfilled", res);
+// });
+
+// promise.catch((err) => {
+//   console.log("promise rejected", err);
+// });
 
 // function getData(dataId, getNextData) {
 //   return new Promise((resolve, reject) => {
@@ -16,3 +26,51 @@ const getPromise = () => {
 //     }, 5000);
 //   });
 // }
+
+// Promise Chain...
+
+// function asyncFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Some Data1");
+//       resolve("success");
+//     }, 4000);
+//   });
+// }
+
+// function asyncFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Some Data2");
+//       resolve("success");
+//     }, 4000);
+//   });
+// }
+
+// console.log("fetching data1...");
+// asyncFunc1().then((res) => {
+//   console.log("fetching data2...");
+//   asyncFunc2().then((res) => {});
+// });
+
+// function getData(dataId, getNextData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataId);
+//       resolve("success");
+//     }, 2000);
+//   });
+// }
+
+// // Promises Chain...
+
+// getData(1)
+//   .then(() => {
+//     return getData(2);
+//   })
+//   .then(() => {
+//     return getData(3);
+//   })
+//   .then(() => {
+//     console.log("success");
+//   });
